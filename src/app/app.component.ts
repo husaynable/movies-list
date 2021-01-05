e357f80dea02d968f212f0c67ac0bbe2a32ced09
Husain Esambaev <husainesambaev@gmail.com>
2021-01-05 11:12:06 +0300

    hiding unnecessary search circle btn by IntersectionObserver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgZone, OnInit, OnDestroy } from '@angular/core';
+import { Component, NgZone, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { fromEvent, Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
@@ -9,13 +9,22 @@ import { MoviesSearchComponent } from './components/movies-search/movies-search.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   showFab = false;
   headerFontSub: Subscription;
+  showCircleBtn = false;
 
   constructor(private matDialog: MatDialog, private ngZone: NgZone) { }
 
   ngOnInit() {
+    this.registerFontSizeChangeObserver();
+  }
+
+  ngAfterViewInit() {
+    this.registerSearchBtnIntersectionObserver();
+  }
+
+  registerFontSizeChangeObserver() {
     this.ngZone.runOutsideAngular(() => {
       this.headerFontSub = fromEvent(document, 'scroll').pipe(
         filter(() => window.scrollY > 42 && window.scrollY < 110)
@@ -27,6 +36,21 @@ export class AppComponent implements OnInit, OnDestroy {
         requestAnimationFrame(updateTitleSize);
       });
     });
+  }
+
+  registerSearchBtnIntersectionObserver() {
+    const callback: IntersectionObserverCallback = (entries) => {
+      const entry = entries[0];
+      if (entry) {
+        if (entry.isIntersecting) {
+          this.showCircleBtn = false;
+        } else {
+          this.showCircleBtn = true;
+        }
+      }
+    };
+    const observer = new IntersectionObserver(callback, {threshold: 0.5});
+    observer.observe(document.getElementById('defaultSearchBtn'));
   }
 
   openSearchDialog() {
