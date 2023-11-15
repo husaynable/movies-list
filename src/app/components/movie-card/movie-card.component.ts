edf525590793c1a38bec3d8100e6c620ceb149d5
Husain Esambaev <husainesambaev@gmail.com>
2023-11-15 18:31:59 +0300

    prettier precommit test

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,32 +1,26 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { MatCardModule } from "@angular/material/card";
-import { NgOptimizedImage } from "@angular/common";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { NgOptimizedImage } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
-import { MovieDetails } from "../../models/movie-details";
+import { MovieDetails } from '../../models/movie-details';
 
 @Component({
-  selector: "app-movie-card",
-  templateUrl: "./movie-card.component.html",
-  styleUrls: ["./movie-card.component.scss"],
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
   standalone: true,
-  imports: [
-    NgOptimizedImage,
-    MatCardModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [NgOptimizedImage, MatCardModule, MatMenuModule, MatButtonModule, MatIconModule],
 })
 export class MovieCardComponent {
   @Input() movie?: MovieDetails;
   @Output() delete = new EventEmitter<MovieDetails>();
 
-  readonly imdbEndpoint = "https://www.imdb.com/title/";
+  readonly imdbEndpoint = 'https://www.imdb.com/title/';
 
-       deleteMovie() {
+  deleteMovie() {
     this.delete.emit(this.movie);
   }
 }
