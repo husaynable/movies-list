b0f576ec3d5c4bd19bae979c7e2eb5f32bdcd56f
Husain Esambaev <husainesambaev@gmail.com>
2023-11-17 01:15:57 +0300

    Update Cypress and Jest configurations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { describe, beforeEach, it, expect } from '@jest/globals';
+import { By } from '@angular/platform-browser';
+import { describe, beforeEach, it, expect, jest } from '@jest/globals';
+import { MatDialog } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let mockFn: jest.MockedFunction<() => void>;
 
   beforeEach(async () => {
+    mockFn = jest.fn();
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    })
+      .overrideComponent(AppComponent, {
+        set: {
+          providers: [{ provide: MatDialog, useValue: { open: mockFn } }],
+        },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +31,10 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call modal open method', () => {
+    fixture.debugElement.query(By.css('.searchBtn')).triggerEventHandler('click');
+    expect(mockFn).toHaveBeenCalled();
   });
 });
